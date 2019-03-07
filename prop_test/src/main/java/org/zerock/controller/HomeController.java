8135b08172bc0a96c965ea53082e8e9aac622b60
zzz <zzz@zzz-GL63-8RD>
2019-03-08 00:49:51 +0900

    TEST Properties with Spring...

---
+++ prop_test/src/main/java/org/zerock/controller/HomeController.java
@@ -0,0 +1,51 @@
+package org.zerock.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@Value("#{socketInfo['ip']}")
+	private String ip;
+	
+	@Value("#{socketInfo['port']}")
+	private int port;
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		logger.info("SOCKET INFO===================");
+		logger.info("ip: "+ip);
+		logger.info("port: "+ port);
+		logger.info("SOCKET INFO===================");
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
---
+++ prop_test/src/main/java/org/zerock/sample/SampleData.java
@@ -0,0 +1,12 @@
+package org.zerock.sample;
+
+import lombok.Data;
+
+@Data
+public class SampleData {
+
+	private String name;
+	private String addr;
+	private String tel;
+	
+}
